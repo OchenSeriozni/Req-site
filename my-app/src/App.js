67cbb35e62abcae50969f341ad17c04f729f1b82
OchenSeriozni <kosarev0726@gmail.com>
2022-09-26 21:49:50 +0300

    Stage #0

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import {Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+
+
+function OutlineTypesExample() {
+  return (
+    <>
+  <h1>Hi</h1>
+  <Button variant='outline-dark'>Кнопка</Button>
+    </>
+  );
+}
+
+export default OutlineTypesExample;
