bd5aa3464031d20e1cb5cccc65d582c471024415
OchenSeriozni <kosarev0726@gmail.com>
2022-09-27 06:04:55 +0300

    stage #1 header/navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,14 @@
 
 import './App.css';
-import {Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-
+import Heder from './components/header.js';
 
 function OutlineTypesExample() {
   return (
     <>
-  <h1>Hi</h1>
-  <Button variant='outline-dark'>Кнопка</Button>
-    </>
+    <Heder></Heder>
+  </>
   );
 }
 
